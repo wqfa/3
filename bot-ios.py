8fe6709d836a828696790e18faa1a2d4b1f67cc0
XF <118357460+wqfa@users.noreply.github.com>
2023-10-27 01:18:37 +0300

    Update bot-ios.py

--- bot-ios.py
+++ bot-ios.py
@@ -411,7 +411,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 ],
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            await update.message.reply_text(". 🦅 مرحبا بك في سورس التجميع الخاص ببوتات الجناح - :\n\n- اشترك في قناة تحديثات بوت التجميع : @Echo_Auto\n\n- سرعة التجميع : " + str(info["sleeptime"]), reply_markup=reply_markup)
+            await update.message.reply_text(". 🦅 مرحبا بك في سورس التجميع الخاص ببوتات الجناح - :\n\n . 🦅 اشترك في قناة تحديثات بوت التجميع - : @DevEviI 🦅 .\n\n- سرعة التجميع : " + str(info["sleeptime"]), reply_markup=reply_markup)
         elif str(update.message.chat.id) in info["admins"]:
             what_need_to_do_echo[str(update.message.chat.id)] = ""
             keyboard = [
